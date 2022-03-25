af258e5a77f46f3ccce04447f496c6c17a6e30f8
Jood80 <shawar.nujood@gmail.com>
2022-03-25 20:36:02 +0200

    feat(confirm): create confirm PIN code page

---
+++ src/app/pages/confirm-pin/confirm-pin.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ConfirmPinPageRoutingModule } from './confirm-pin-routing.module';
+
+import { ConfirmPinPage } from './confirm-pin.page';
+
+@NgModule({
+  imports: [CommonModule, IonicModule, ConfirmPinPageRoutingModule],
+  declarations: [ConfirmPinPage],
+})
+export class ConfirmPinPageModule {}
---
+++ src/app/pages/confirm-pin/confirm-pin.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-pin',
+  templateUrl: './confirm-pin.page.html',
+  styleUrls: ['./confirm-pin.page.scss'],
+})
+export class ConfirmPinPage {
+  constructor() {}
+}
