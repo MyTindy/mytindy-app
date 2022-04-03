211f04dc4a08a0ca94c011c8065209c136f68f8d
NadiaJalalTamimi <nadia.tamimi74@gmail.com>
2022-04-03 11:40:27 +0300

    feat(login): create login functionality.

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import firebase from 'firebase/compat/app';
+import { FormBuilder, FormGroup, Validators,
+} from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -9,18 +12,48 @@ import { AuthService } from 'src/app/services/auth.service';
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage {
-  OPT: string = '';
+export class LoginPage  {
+  // form: FormGroup;
+  opt = '';
+  loading=false;
+  serverMessage='';
   phoneNumber: string;
+  formType: 'log in' | 'Register' | 'reset' = 'Register';
 
   recaptchaVerifier: firebase.auth.RecaptchaVerifier;
   confirmationResult: any;
 
   constructor(private authService: AuthService, private router: Router) {}
 
+  get isLogin(): boolean {
+    return this.formType === 'log in';
+  }
+
+  get isSignup(): boolean {
+    return this.formType === 'Register';
+  }
+  // get name(){
+  //   return this.form.get('name');
+  // }
+  // get phone(){
+  //   return this.form.get('phone');
+  // }
+
+  // ngOnInit(){
+  //   this.form =this.fb.group({
+  //     name:['',[Validators.required, Validators.name]],
+  //     phone:['',[Validators.required]]
+  //   });
+  // }
+
+
+  changeFormType(value: 'log in' | 'Register' | 'reset' = 'Register') {
+    this.formType = value;
+  }
+
   async ionViewDidEnter() {
     this.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
-      'sign-in-button',
+      'submit-button',
       {
         size: 'invisible',
         callaback: (res) => {},
@@ -31,7 +64,7 @@ export class LoginPage {
 
   ionViewDidLoad() {
     this.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
-      'sign-in-button',
+      'submit-button',
       {
         size: 'invisible',
         callaback: (res) => {},
@@ -42,16 +75,18 @@ export class LoginPage {
 
   onPhoneEmitted($event) {
     this.phoneNumber = $event;
+    console.log(this.phoneNumber);
   }
 
   signinWithPhoneNumber($event) {
+    console.log($event);
     try {
       if (this.phoneNumber) {
+        console.log(this.phoneNumber);
         this.authService
           .signInWithPhoneNumber(this.recaptchaVerifier, this.phoneNumber)
           .then((success) => {
             console.log('country', this.recaptchaVerifier);
-
             this.router.navigate(['/confirm']);
           });
       }
@@ -59,4 +94,21 @@ export class LoginPage {
       console.error('error', error);
     }
   }
+// async onSubmit(){
+// this.loading = true;
+// const name = this.name.value;
+// const phone = this.phoneNumber;
+// try{
+// if (this.isLogin){
+
+// await this.authService.signInWithPhoneNumber(this.recaptchaVerifier,phone);
+// }if (this.isSignup){
+//   await this.authService.signInWithPhoneNumber(this.recaptchaVerifier,phone);
+// }
+// }catch(err){
+//   this.serverMessage = err;
+// }
+// this.loading = false;
+// }
+
 }
