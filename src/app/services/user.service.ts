7801e00c224df3cc6c9a4693ec987242c4865810
NadiaJalalTamimi <nadia.tamimi74@gmail.com>
2022-04-14 18:18:59 +0300

    chore(service): user service CRUD.

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,55 +1,53 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
-  constructor(private firestore: AngularFirestore) {}
+  userInfo: any;
+  constructor(
+    private firestore: AngularFirestore,
+    private afAuth: AngularFireAuth
+  ) {}
 
-  // getUsers(usersData){
-  //   // const userRef = collection(this.firestore , 'users');
-  //   // return collectionData(userRef, {idField: 'id'}) as  Observable<UsersData[]>;
-  //   return this.firestore.collection('users').add({ uid, name ,email, code , photoURL , phoneNumber, location });
-  // }
-
-    getUserById(uid){
-      return this.firestore.collection('users').doc(uid).get({name ,email,phone});
-      // const userDocRef = doc(this.firestore, 'users/${id}');
-      // return docData(userDocRef, {idField: 'id'}) as  Observable<UsersData[]>;
-    }
-  addUser(userData, location) {
-    console.log(userData);
-    console.log(location);
-
+  createUser(userData, location) {
     const { uid, phoneNumber, displayName, email } = userData;
     return this.firestore
       .collection('users')
       .add({ uid, phone: phoneNumber, name: displayName, email, location });
   }
 
-  deleteUser(userdata) {
-    // const userDocRef = doc(this.firestore, 'users/${usersData.id}');
-    // return deleteDoc(userDocRef);
-    return this.firestore.collection('users').doc(userdata.uid).delete();
+  getUserById() {
+    try {
+      return this.afAuth.authState.pipe(
+        switchMap((user) => {
+          if (user) {
+            return this.firestore
+              .collection<any>('users', (ref) =>
+                ref.where('uid', '==', user.uid)
+              )
+              .valueChanges({ idField: 'id' });
+          } else {
+            return [];
+          }
+        })
+      );
+    } catch (error) {
+      console.log(error);
+    }
   }
+
   updateUser(userdata) {
     return this.firestore
       .collection('users')
       .doc(userdata.uid)
       .update({ userdata });
-    // const userDocRef = doc(this.firestore, 'users/${usersData.id}');
-    // return updateDoc(userDocRef,{ name: userdata.name,
-    //   code: userdata.code,
-    //   photoURL: userdata.photoURL,
-    //   phoneNumber: userdata.phoneNumber, location:userdata.location});
+  }
+
+  deleteUser(id) {
+    return this.firestore.collection('users').doc(id).delete();
   }
 }
-// try {
-//   return this.firestore
-//     .collection('users')
-//     .add({ uid, name, phone, email, location, photoURL });
-// } catch (error) {
-//   console.error('User has not been created', error);
-// }
