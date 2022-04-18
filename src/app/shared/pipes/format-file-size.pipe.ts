918e706463939c1209eff50670ce6cd8b38c727c
Jood80 <shawar.nujood@gmail.com>
2022-04-18 16:22:08 +0300

    feat(drag): add drag&drop zone directive for images (#53)

--- src/app/shared/pipes/format-file-size.pipe.ts
+++ src/app/shared/pipes/format-file-size.pipe.ts
@@ -14,7 +14,7 @@ const FILE_SIZE_UNITS_LONG = [
   name: 'formatFileSize',
 })
 export class FormatFileSizePipe implements PipeTransform {
-  transform(sizeInBytes: number, longForm: boolean): string {
+  transform(sizeInBytes: number, longForm?: boolean): string {
     const units = longForm ? FILE_SIZE_UNITS_LONG : FILE_SIZE_UNITS;
     let power = Math.round(Math.log(sizeInBytes) / Math.log(1024));
     power = Math.min(power, units.length - 1);
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,14 +4,21 @@ import { CommonModule } from '@angular/common';
 import { GoogleLoginDirective } from './directives/google-login.directive';
 import { FacebookLoginDirective } from './directives/facebook-login.directive';
 import { FormatFileSizePipe } from './pipes/format-file-size.pipe';
+import { DropzoneDirective } from './directives/dropzone.directive';
 
 @NgModule({
   declarations: [
     GoogleLoginDirective,
     FacebookLoginDirective,
+    DropzoneDirective,
     FormatFileSizePipe,
   ],
   imports: [CommonModule],
-  exports: [GoogleLoginDirective, FacebookLoginDirective, FormatFileSizePipe],
+  exports: [
+    GoogleLoginDirective,
+    FacebookLoginDirective,
+    DropzoneDirective,
+    FormatFileSizePipe,
+  ],
 })
 export class SharedModule {}
