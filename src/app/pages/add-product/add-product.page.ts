05c63cc4114c286fdba0c985ec5c3768b9911e1a
Jood80 <shawar.nujood@gmail.com>
2022-04-23 18:11:57 +0300

    chore(service): GET the products

--- src/app/pages/add-product/add-product.page.ts
+++ src/app/pages/add-product/add-product.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-add-product',
@@ -7,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProductPage implements OnInit {
   isHovering: boolean;
+  mockData: any;
 
   files: File[] = [];
 
-  constructor() {}
+  constructor(private productService: ProductService) {}
 
   ngOnInit() {}
   toggleHover(event: boolean) {
@@ -21,5 +23,11 @@ export class AddProductPage implements OnInit {
     for (let i = 0; i < files.length; i++) {
       this.files.push(files.item(i));
     }
+  }
+
+  getProducts() {
+    this.productService.getProduct().subscribe((data) => {
+      console.log(data);
+    });
   }
 }
