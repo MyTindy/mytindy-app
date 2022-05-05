f84309bebb0f36a56cb21cb8ecd38fe208f50b3a
Jood80 <shawar.nujood@gmail.com>
2022-05-05 15:16:46 +0300

    chore(products): implement proudct CRUD

--- src/app/pages/add-product/add-product.page.ts
+++ src/app/pages/add-product/add-product.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { ProductService } from 'src/app/services/product.service';
+import { UsersService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-add-product',
@@ -9,10 +11,14 @@ import { ProductService } from 'src/app/services/product.service';
 export class AddProductPage implements OnInit {
   isHovering: boolean;
   mockData: any;
+  sellers;
 
   files: File[] = [];
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    public usersService: UsersService
+  ) {}
 
   ngOnInit() {}
   toggleHover(event: boolean) {
@@ -29,5 +35,43 @@ export class AddProductPage implements OnInit {
     this.productService.getProduct().subscribe((data) => {
       console.log(data);
     });
+  }
+
+  postProduct() {}
+
+  getSellers() {
+    this.usersService.getSellers().subscribe((data) => (this.sellers = data));
+  }
+
+  postSeller() {
+    const info = {
+      sp_store_name: 'development',
+      seller_name: 'Nujood2',
+      email: 'shwar.nujood@gmail.com',
+      password: 'Nujood@2020',
+    };
+    this.usersService.createSellers(info).subscribe((res) => console.log(res));
+  }
+
+  getSeller() {
+    this.usersService.getSeller(1043962).subscribe((res) => console.log(res));
+  }
+
+  updateSeller() {
+    const info = {
+      sp_store_name: 'development',
+      seller_name: 'Jood',
+      email: 'nujood@gmail.com',
+      password: 'Nujood@2020',
+    };
+    this.usersService
+      .updateSeller(1043962, info)
+      .subscribe((res) => console.log(res));
+  }
+
+  removeSeller() {
+    this.usersService
+      .deleteSeller(1043963)
+      .subscribe((res) => console.log(res));
   }
 }
