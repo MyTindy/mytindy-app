eb01638a36ad79bd0287dacf2afb75732f65b71e
Jood80 <shawar.nujood@gmail.com>
2022-05-10 14:29:22 +0300

    refactor(collection): refactor the collection form compoenent

--- client/src/app/components/collections/collections.component.ts
+++ client/src/app/components/collections/collections.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { COLLECTIONS } from 'src/app/shared/constants/collection.constant';
 
 @Component({
@@ -6,7 +7,30 @@ import { COLLECTIONS } from 'src/app/shared/constants/collection.constant';
   templateUrl: './collections.component.html',
   styleUrls: ['./collections.component.scss'],
 })
-export class CollectionsComponent {
-  collections: string[] = COLLECTIONS;
-  constructor() {}
+export class CollectionsComponent implements OnInit {
+  // collections: string[] = COLLECTIONS;
+  collectionsList = [
+    'Dress',
+    'Pants',
+    'Shorts',
+    'Hat',
+    'Shoes',
+    'Candle',
+    'Jewelry',
+    'Home Decor',
+    'Jewelry Box',
+    'Bags',
+  ];
+  collectionsForm: FormGroup;
+
+  constructor(public formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.collectionsForm = this.formBuilder.group({
+      collections: [''],
+    });
+  }
+  onSubmit() {
+    console.log(this.collectionsForm);
+  }
 }
