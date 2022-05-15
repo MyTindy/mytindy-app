0501493cecb15570a55c953ac28400a60a6ce741
Jood80 <shawar.nujood@gmail.com>
2022-05-15 16:35:20 +0300

    refactor(product): create form of product-details with validation

--- client/src/app/components/product-details/product-details.component.ts
+++ client/src/app/components/product-details/product-details.component.ts
@@ -1,12 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormArray,FormControl,FormGroup,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.scss'],
 })
-export class ProductDetailsComponent implements OnInit {
-  constructor() {}
+export class ProductDetailsComponent {
+  detailsForm: FormGroup;
+  colorOptions: string[] = ['red','green','orange','blue'];
 
-  ngOnInit() {}
+  constructor() {
+    this.detailsForm = new FormGroup({
+      description: new FormControl('',Validators.required),
+      colors: new FormArray([]),
+      price: new FormControl(0,Validators.required),
+    });
+    this.colorOptions?.forEach(() => this.detailsFormArray?.push(new FormControl(false)));
+  }
+
+  get detailsFormArray() {
+    return this.detailsForm.controls.colors as FormArray;
+  }
+
+  submitForm() {
+    console.log(this.detailsForm.value);
+  }
 }
