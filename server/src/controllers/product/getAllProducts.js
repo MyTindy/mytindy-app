95fcaa2a1ddb56c06cdc69ad3823b7bbf1c0c5f0
Jood80 <shawar.nujood@gmail.com>
2022-05-16 00:49:04 +0300

    style(controllers): move all product controllers into `product` dir

--- server/src/controllers/product/getAllProducts.js
+++ server/src/controllers/product/getAllProducts.js
@@ -1,6 +1,6 @@
-module.exports = async (_req, res) => {
+module.exports=async (_req, res) => {
   try {
-    let header = {
+    let header={
       method: "GET",
       headers: {
         Accept: "application/json",
@@ -8,13 +8,13 @@ module.exports = async (_req, res) => {
         Authorization: `Bearer ${process.env.TOKEN}`,
       },
     };
-    const data = await fetch(
+    const data=await fetch(
       `${process.env.BASE_URL}/api/v2/products.json`,
       header
     );
-    const products = await data.json();
-    return res.status(200).json({ products });
-  } catch (error) {
-    return res.status(500).json({ message: "Failed to fetch" });
+    const products=await data.json();
+    return res.status(200).json({products});
+  } catch(error) {
+    return res.status(500).json({message: "Failed to fetch"});
   }
 };
