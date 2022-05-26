a57a1dbab2d8d354af51fd48d7114117fe9c916b
NadiaJalalTamimi <nadia.tamimi74@gmail.com>
2022-05-26 10:54:34 +0300

    feat(card): search & delete card functionality.

--- client/src/app/components/card/card.component.ts
+++ client/src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { products } from 'src/app/shared/constants/card-component.constants';
 
 @Component({
@@ -6,12 +6,26 @@ import { products } from 'src/app/shared/constants/card-component.constants';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
-export class CardComponent implements OnInit {
+export class CardComponent implements OnChanges{
+  @Input() searchProduct: string;
+  productsList;
+filtredList = [];
+  constructor() {}
 
-  productsList = products;
-
-  constructor() { }
-
-  ngOnInit() {}
+ngOnChanges(changes: SimpleChanges) {
+  console.log(changes.searchProduct.currentValue);
+  if(!this.searchProduct.trim()){
+this.productsList = products;
+  }else{
+    this.productsList = this.onSearchProduct(products);
+  }
+}
+onSearchProduct(productsArr){
+return productsArr.filter(product => product.name.toLowerCase().includes(this.searchProduct.trim().toLowerCase()));
+}
+onDeleteCard(productIndex){
+console.log(productIndex);
+this.productsList.splice(productIndex,1);
+}
 
 }
