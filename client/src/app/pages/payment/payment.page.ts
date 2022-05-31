4149a7786448cdfb5d077c7ef54f006a17905f4a
Jood80 <shawar.nujood@gmail.com>
2022-05-31 20:02:04 +0300

    chore(select): select dummy data of items

--- client/src/app/pages/payment/payment.page.ts
+++ client/src/app/pages/payment/payment.page.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment.page.scss'],
 })
 export class PaymentPage implements OnInit {
+  selectedValue=1;
+  data = [
+    {
+      id: 1,
+      name: 'name1'
+    },
+    {
+      id: 2,
+      name: 'name2'
+    },
+  ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onChange(event) {
+    console.log(event.target.value);
   }
 
 }
