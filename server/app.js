bb29501b8445322deb6e68b6f8e5f8ed5ff8ec32
Jood80 <shawar.nujood@gmail.com>
2022-06-14 16:07:04 +0300

    chore(*): remove android files & unneeded packages

--- server/app.js
+++ server/app.js
@@ -1,20 +1,22 @@
-const { join } = require('path')
-const express = require('express')
-const cors = require('cors')
-const router = require('./src/routes')
+const {join}=require('path')
+const express=require('express')
+const cors=require('cors')
+const router=require('./src/routes')
 
-const app = express()
+const app=express()
 
-app.use(express.static(join(__dirname, '..', 'dist', 'mytindy')))
+// app.use(express.static(join(__dirname, '..', 'client', 'www')))
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({extended: false}))
 
 app.use(cors())
-
+app.use('/', (req, res) => {
+  res.send({message: "Hello World"})
+})
 app.use('/api', router);
 
-app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'dist', 'mytindy-app', 'index.html'))
-})
+// app.get('*', (_req, res) => {
+//   res.sendFile(join(__dirname, '..', 'client/www', 'index.html'))
+// })
 
-module.exports = app
+module.exports=app
