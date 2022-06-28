3c2e9fe07cb97ba5eccafbd011b79949d9d60497
Jood80 <shawar.nujood@gmail.com>
2022-06-28 22:11:42 +0300

    fix(voice): tweak the quality of the voice reader

--- client/src/app/services/text-to-speech.service.ts
+++ client/src/app/services/text-to-speech.service.ts
@@ -13,9 +13,6 @@ export class TextToSpeechService {
   initSynthesis(): void {
     this.speechSynthesizer = new SpeechSynthesisUtterance();
     this.speechSynthesizer.voice = window.speechSynthesis.getVoices()[2];
-    this.speechSynthesizer.volume = 1;
-    this.speechSynthesizer.rate = 1;
-    this.speechSynthesizer.pitch = 0.2;
   }
 
   speak(word: string, language: string): void {
